379e82fb658b9376854b597193a8b34036a6c7a9
Teemu Turunen <teemu.turunen@gmail.com>
2018-04-07 20:20:40 +0300

    New gesture: duck

---
+++ InMoov/gestures/ankka.py
@@ -0,0 +1,65 @@
+def ankka():
+
+  i01.startedGesture()
+
+  fullspeed()
+
+  # make fists
+
+  print commands.getoutput('/bin/echo "50,50,50,50,50" | /usr/bin/nc localhost 10000')
+  print commands.getoutput('/bin/echo "50,50,50,50,50" | /usr/bin/nc localhost 10001')
+
+  # palms down
+
+  i01.moveHand("right",180,180,180,180,180,137)
+  i01.moveHand("left",180,180,180,180,180,80)
+
+  # arms up
+
+  i01.moveArm("left", 80, 84, 130, 90)
+  i01.moveArm("right", 90, 90, 140, 97)
+
+  sleep(5)
+
+  # do the shake
+
+  i01.moveArm("left", 65, 84, 120, 90)
+  i01.moveArm("right", 90, 90, 140, 97)
+
+
+  sleep(0.5)
+
+  i01.moveArm("left", 80, 84, 130, 90)
+  i01.moveArm("right", 75, 90, 130, 97)
+
+  sleep(0.5)
+
+  i01.moveArm("left", 65, 84, 120, 90)
+  i01.moveArm("right", 90, 90, 140, 97)
+
+  sleep(0.5)
+
+  i01.moveArm("left", 80, 84, 130, 90)
+  i01.moveArm("right", 75, 90, 130, 97)
+
+  sleep(0.5)
+
+  i01.moveArm("left", 65, 84, 120, 90)
+  i01.moveArm("right", 90, 90, 140, 97)
+
+  sleep(2)
+
+  # rest
+
+  i01.moveArm("left", 8, 90, 90, 90)
+  i01.moveArm("right", 20, 90, 90, 97)
+
+  i01.moveHand("right",180,180,180,180,180,147)
+  i01.moveHand("left",180,180,180,180,180,70)
+
+  print commands.getoutput('/bin/echo "50,50,50,50,50" | /usr/bin/nc localhost 10000')
+  print commands.getoutput('/bin/echo "50,50,50,50,50" | /usr/bin/nc localhost 10001')
+
+  sleep(2)
+
+  i01.finishedGesture()
